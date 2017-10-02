bc82bf9692db31f408e72e62dea4679867697eb8
student <student@localhost.localdomain>
2017-10-02 01:44:31 -0600

    update

--- shell/test.c
+++ shell/test.c
@@ -6,57 +6,82 @@
 #include "test.h"
 #include <errno.h>
 
+
+int isPipe = 0;
+
 int main(int argc, char **argv, char **envp){
   int size = 1024;
   int i = 0;
   char *str = (char *)malloc(size * sizeof(char));
   char delim = ' ';
-  char *path;
-  char **posPath;
-  char *realPath;
-  char *cmd;
   char **vector;
-  pid_t pid;  
-
+  
   while(1){
     write(1, "$ ", 2);
     scanf("%[^\n]%*c",str);                        //get input from user, avoiding \n
     if(compareWord("exit", str) == 1){
 	return 0;
     }
+    if(compareWord("\n", str) == 1){   //I have to fix this
+      break;
+    }
     vector = mytoc(str, delim);
-    cmd = vector[0];
-    if(access(cmd, F_OK) == 0){                    //access function will check if the path exists, return 0 if it does
+    selectCommand(vector, envp, isPipe, str, delim);
+  }
+  
+  return 0;
+}
+
+/*
+Function will determine which of all the possible commands is the one entered
+ */
+
+int selectCommand(char **vector, char **envp, int isPipe, char *str, char delim){
+
+  char *path;
+  char **posPath;
+  char *realPath;
+  char *temp;
+  pid_t pid;
+
+  if(cdCommand(vector) == 1){                      //checking if the given command is cd
+    if(countWords(str, delim) == 1){              //in case there was not given a directory, emulating the actual cd command it should go up one
+      temp = "..";
+      chdir(temp);
+    }
+    else{                                          //in case there was given a directory to change
+      chdir(vector[1]);
+    }
+  }
+  else if(lookForPipes(str) > 1){            //there are pipes that need to be considered
+    isPipe = 1;
+    
+
+    
+  }
+  else{
+    if(access(vector[0], F_OK) == 0){                    //access function will check if the path exists, return 0 if it does
       pid = saferFork();                           //calling saferFork, from the examples
       if(pid == 0){                                //if it returs no error
-        execve(cmd, vector, envp);                 //call execve, in case there was given a correct path
+	execve(vector[0], vector, envp);                 //call execve, in case there was given a correct path
       }
       else{                                        //in case it is still executing the child process
 	wait(NULL);
       }
-    }
-        
-    while(*envp != (char*)0){                      
-      char ** tempVector = mytoc(envp[i], '=');    //tokenizing environmental variables
-      if(compareWord(tempVector[0], "PATH") == 1){ //looking for the one containing the PATH
-	path = tempVector[1];                      //when found the path, save its contents on var path   
-	break;
       }
-      envp++;
-      free(tempVector);
-    }
+    path = lookForPath(envp);
     posPath = mytoc(path, ':');                    //tokenizing the given path
     while(*posPath){                               //traversing the possible paths
       realPath = concatenate(*posPath, "/");       //adding '/' to the possible paths
-      realPath = concatenate(realPath, cmd);       //adding the given command by the user after '/'
+      realPath = concatenate(realPath, vector[0]);       //adding the given command by the user after '/'
       if(access(realPath, F_OK) == 0){             //access function will check if the new path exists
-	pid = saferFork();
-	if(pid == 0){
-	  execve(realPath, vector, envp);
-	}
-	else{
-	  wait(NULL);
-	}
+	  pid = saferFork();
+	  if(pid == 0){
+	    execve(realPath, vector, envp);
+	  }
+	  else{
+	    wait(NULL);
+	  }
       }  
       free(*posPath);
       posPath++;
@@ -64,6 +89,72 @@ int main(int argc, char **argv, char **envp){
   }
   return 0;
 }
+
+
+/*
+Look for PATH
+ */
+char* lookForPath(char **envp){
+  int i = 0;
+  char *path;
+  while(*envp != (char*)0){                      
+    char ** tempVector = mytoc(envp[i], '=');    //tokenizing environmental variables
+    if(compareWord(tempVector[0], "PATH") == 1){ //looking for the one containing the PATH
+      path = tempVector[1];                      //when found the path, save its contents on var path   
+      break;
+    }
+    envp++;
+    free(tempVector);
+  }
+  return path;
+}
+
+/*
+Function to check if the entered command is cd
+ */
+int cdCommand(char **vector){
+  if(compareWord(vector[0], "cd")){
+    return 1;
+  }
+  return 0;
+}
+
+/*
+In case the entered command words as a pipe
+ */
+char **determinePipes(char *str){
+  int fileD[2];
+  pid_t pid = saferFork();
+  char **vector = mytoc(str, '|');
+  int numWords = countWords(str, '|');
+  int stdIn2 = dup (0);
+  int stdOut2 = dup(1);
+  fileD[0] = 0;
+  fileD[1] = 1;
+  for(int i = 0; i < numWords - 1; i++){
+    pipe(fileD);
+    
+    if(pid == 0){
+      
+    
+      close(fileD[0]);                          //Closing the input side of the pipe
+      dup2(fileD[1], 0);
+    
+    }
+    else{
+      close(fileD[1]);
+    }
+  }
+  return 0;
+}
+
+/*
+Look for pipe passing the command as parameter and looking for '|' char
+ */
+int lookForPipes(char *str){
+  return (countWords(str, '|'));
+}
+
 
 /*
 concatenate function, will create a new char * adding the content 
--- shell/test.h
+++ shell/test.h
@@ -4,5 +4,10 @@
 char *concatenate(char *str, char *pStr);
 int countCharacters(char *str);
 pid_t saferFork();
+char* lookForPath(char **envp);
+int cdCommand(char **vector);
+int determinePipe(char *str);
+int lookForPipes(char *str);
+int selectCommand(char **vector, char **envp, int pipe, char *str, char delim);
 
 #endif
